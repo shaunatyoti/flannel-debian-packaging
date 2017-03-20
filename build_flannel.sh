b8d97624d15c8fe5fca7f9ef2ad54bbea11550d9
shaun gibson <shaun.gibson@yoti.com>
2017-03-20 11:00:41 +0000

    update to v0.7.0

--- build_flannel.sh
+++ build_flannel.sh
@@ -1,6 +1,6 @@
 # build flannel debian package
 
-FLANNEL_VERSION=${FLANNEL_VERSION:-0.6.2}
+FLANNEL_VERSION=${FLANNEL_VERSION:-0.7.0}
 REV=${REV:-1}
 
 rm -f flannel/builds/flannel_$FLANNEL_VERSION_amd64.deb
